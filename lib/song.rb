79e34d967ac0be6a49cab30163e91f48763a2560
Rachel T <rachmaybe@gmail.com>
2020-02-16 03:46:04 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,25 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class Song
-
+  attr_accessor :name, :album
+  attr_reader :id
+ 
+  def initialize(id=nil, name, album)
+    @id = id
+    @name = name
+    @album = album
+  end
+ 
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+        )
+    SQL
+    DB[:conn].execute(sql)
+  end
 
   def self.table_name
     self.to_s.downcase.pluralize
